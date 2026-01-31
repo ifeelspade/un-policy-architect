94c4022ca349a48b03697de9c8907de6c254f548
ifeelspade <kulshresthauk71@gmail.com>
2026-01-31 20:46:20 +0530

    Update basetrial2.py

--- basetrial2.py
+++ basetrial2.py
@@ -71,8 +71,7 @@ TEAM_CREDENTIALS = {
     "Big Brain Energy": "bigbrainenergy@2050",
     "KINTSUGI": "kintsugi@2050",
     "Alchemists": "alchemists@2050",
-    "spade papa": "spadepapa@2050",
-    "akash surwase": "akashsurwase@2050"
+    "spade papa": "spadepapa@2050"
 }
 
 
@@ -623,6 +622,7 @@ elif st.session_state.game_over:  # <--- FIXED: using st.session_state.year
     st.success(f"🏆 SIMULATION COMPLETE. Final Sustainability Score: {score:.0f}")
     st.balloons()
     st.session_state.game_over = True	
+
 
 
 
